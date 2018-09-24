76724a5f3a4509fe79e2160782a3f0709c64b36f
Vyas Raina <sayvaniar@gmail.com>
2018-09-24 15:16:17 +0100

    added some questions

--- questions.js
+++ questions.js
@@ -3,7 +3,7 @@ data= [
         'category': 'Youth',
         'categoryQuestions': [
             {
-                'question': 'What year was Sandeep born in?',
+                'question': 'In which year was MLK assassinated and Sandeep born?',
                 'A': '1968',
                 'B': '1972',
                 'C': '1967',
@@ -30,21 +30,21 @@ data= [
             },
         
             {
-                'question': 'my fourth question',
-                'A': 'option A',
-                'B': 'option B',
-                'C': 'option C',
-                'D': 'option D',
-                'correct': 'A'
+                'question': 'Which primary school did Sandeep go to?',
+                'A': 'St Paul\'s',
+                'B': 'Baramulla Primary',
+                'C': 'St Joseph\'s',
+                'D': 'Home-schooled',
+                'correct': 'C'
             },
         
             {
-                'question': 'my fifth question',
-                'A': 'option A',
-                'B': 'option B',
-                'C': 'option C',
-                'D': 'option D',
-                'correct': 'A'
+                'question': 'Name the river flowing through the city of Sandeep\'s University?',
+                'A': 'Ravi',
+                'B': 'Chenab',
+                'C': 'Jhelum',
+                'D': 'Bhramaputra',
+                'correct': 'C'
             },
         
             {
@@ -98,21 +98,21 @@ data= [
         'category': 'Home-Life',
         'categoryQuestions': [
             {
-                'question': 'What year was Sandeep born in?',
-                'A': '1968',
-                'B': '1972',
-                'C': '1967',
-                'D': '1969',
-                'correct': 'A'
+                'question': 'Which is Sandeep\'s favourite cereal bar?',
+                'A': 'Belvita',
+                'B': 'Naked',
+                'C': 'Alpen',
+                'D': 'Eat Natural',
+                'correct': 'D'
             },
         
             {
-                'question': 'my second question',
-                'A': 'option A',
-                'B': 'option B mate',
-                'C': 'option C',
-                'D': 'option D',
-                'correct': 'A'
+                'question': 'Where would you find Sandeep on a typical Saturday afternoon?',
+                'A': 'The Mall',
+                'B': 'In his garden',
+                'C': 'Office',
+                'D': 'Beach',
+                'correct': 'B'
             },
         
             {
@@ -193,20 +193,20 @@ data= [
         'category': 'Lifestyle-and-Culture',
         'categoryQuestions': [
             {
-                'question': 'What year was Sandeep born in?',
-                'A': '1968',
-                'B': '1972',
-                'C': '1967',
-                'D': '1969',
+                'question': 'Translated into English the name of Sandeep\'s favourite show is: Life is fruitful. What is the country of origin of this show??',
+                'A': 'Pakistan',
+                'B': 'India',
+                'C': 'Russia',
+                'D': 'Nepal',
                 'correct': 'A'
             },
         
             {
-                'question': 'my second question',
-                'A': 'option A',
-                'B': 'option B mate',
-                'C': 'option C',
-                'D': 'option D',
+                'question': 'Sandeep\'s current dog and first car have one common property. What is it?',
+                'A': 'Colour',
+                'B': 'Name',
+                'C': 'Age when bought',
+                'D': 'Country of origin',
                 'correct': 'A'
             },
         
@@ -288,48 +288,48 @@ data= [
         'category': 'Work-Life',
         'categoryQuestions': [
             {
-                'question': 'What year was Sandeep born in?',
-                'A': '1968',
-                'B': '1972',
-                'C': '1967',
-                'D': '1969',
+                'question': 'Name the currency used by the country in which Sandeep worked for 3 years?',
+                'A': 'Dollar',
+                'B': 'Euro',
+                'C': 'Yen',
+                'D': 'Lira',
+                'correct': 'D'
+            },
+        
+            {
+                'question': 'At a humble age of 21, Sandeep graduated and joined his first job in Delhi. Which company was this?',
+                'A': 'C-Dot',
+                'B': 'InfoVista',
+                'C': 'Huawei',
+                'D': 'Ericsson',
                 'correct': 'A'
             },
         
             {
-                'question': 'my second question',
-                'A': 'option A',
-                'B': 'option B mate',
-                'C': 'option C',
-                'D': 'option D',
+                'question': 'Ambitious as ever, Sandeep made another leap of faith from Istanbul. How many miles did he travel with his family to his new home?',
+                'A': '20',
+                'B': '200',
+                'C': '2000',
+                'D': '20,000',
+                'correct': 'C'
+            },
+        
+            {
+                'question': 'As of his 50th Birthday, which greeting below belongs to the language of the country from which Sandeep\'s current company originates?',
+                'A': 'Bonjour',
+                'B': 'Hallo',
+                'C': 'Ni Hao',
+                'D': 'Ciao',
                 'correct': 'A'
             },
         
             {
-                'question': 'my third question',
-                'A': 'option A',
-                'B': 'option B',
-                'C': 'option C',
-                'D': 'option D',
-                'correct': 'A'
-            },
-        
-            {
-                'question': 'my fourth question',
-                'A': 'option A',
-                'B': 'option B',
-                'C': 'option C',
-                'D': 'option D',
-                'correct': 'A'
-            },
-        
-            {
-                'question': 'my fifth question',
-                'A': 'option A',
-                'B': 'option B',
-                'C': 'option C',
-                'D': 'option D',
-                'correct': 'A'
+                'question': 'Work and travel come hand in hand for Sandeep! One of these countries is both "South and North", as well as having a coast facing the world\'s third largest ocean - which country is this?',
+                'A': 'Brazil',
+                'B': 'Gabon',
+                'C': 'Indonesia',
+                'D': 'Kenya',
+                'correct': 'D'
             },
         
             {
@@ -383,12 +383,12 @@ data= [
         'category': 'Miscellaneous',
         'categoryQuestions': [
             {
-                'question': 'What year was Sandeep born in?',
-                'A': '1968',
-                'B': '1972',
-                'C': '1967',
-                'D': '1969',
-                'correct': 'A'
+                'question': 'What is Sandeep\'s favourite colour?',
+                'A': 'Red',
+                'B': 'Blue',
+                'C': 'Black',
+                'D': 'Green',
+                'correct': 'B'
             },
         
             {
